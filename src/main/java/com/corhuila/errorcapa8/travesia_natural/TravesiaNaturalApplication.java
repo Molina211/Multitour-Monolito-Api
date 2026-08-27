14db12fa7e1b2287e3180589b04d686291f2ec97
Molina211 <jhonmolina21109@gmail.com>
2026-08-27 08:14:27 -0500

    ✨ Creacion de la carpeta Springboot con dependencias basicas

---
+++ src/main/java/com/corhuila/errorcapa8/travesia_natural/TravesiaNaturalApplication.java
@@ -0,0 +1,13 @@
+package com.corhuila.errorcapa8.travesia_natural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravesiaNaturalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravesiaNaturalApplication.class, args);
+	}
+
+}
